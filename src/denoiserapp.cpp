877d027ed88d7a52022ddefd95e415195fae2ae7
Joseph-Heetel <joseph.heetel@protonmail.com>
2022-11-29 18:06:11 +0100

    Add required extension (VK_KHR_push_descriptor)

--- src/denoiserapp.cpp
+++ src/denoiserapp.cpp
@@ -32,6 +32,7 @@ namespace denoise {
         pds.add_required_extension(VK_KHR_TIMELINE_SEMAPHORE_EXTENSION_NAME);
         pds.add_required_extension(VK_KHR_EXTERNAL_MEMORY_EXTENSION_NAME);
         pds.add_required_extension(VK_KHR_GET_MEMORY_REQUIREMENTS_2_EXTENSION_NAME);
+        pds.add_required_extension(VK_KHR_PUSH_DESCRIPTOR_EXTENSION_NAME);
 #ifdef WIN32
         pds.add_required_extension(VK_KHR_EXTERNAL_SEMAPHORE_WIN32_EXTENSION_NAME);
         pds.add_required_extension(VK_KHR_EXTERNAL_MEMORY_WIN32_EXTENSION_NAME);
